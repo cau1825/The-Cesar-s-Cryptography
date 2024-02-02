0313e63f89f1e70a3bbb6cddd38c2c6adcfb9ada
cau1825 <benochavez93@gmail.com>
2024-02-02 16:07:28 -0600

    Creacion de cifrado Cesar 13 en Python

---
+++ Cesar13.py
@@ -0,0 +1,28 @@
+
+def cifradoCesar13(MensajeInput):
+    #Se declara la variable mensaje para guardar el texto recibido
+    mensaje = MensajeInput
+    #La variable diccionario cuenta con las letras del alfabeto
+    diccionario="abcdefghijklmnopqrstuvwxtz"
+    #La variable auxiliar nos permite guardar la iteraccion para crear el mensaje encriptado
+    auxiliar=""
+    for i in mensaje:
+        #se lee cada elemento  de la variable  para buscarlo en la variable diccionario se suman 13 ya que esa es la clave del cifrado y usa el modulo para estar iterando en el diccionario
+        auxiliar = auxiliar +  diccionario[(diccionario.find(i) + 13) % len(diccionario)]
+    print(f"El mensaje cifrado es: {auxiliar}")
+    
+def descifradoCesar13(MensajeInputCifrado):
+    #Se declara la variable mensaje para guardar el texto recibido
+    mensaje = MensajeInputCifrado
+    #La variable diccionario cuenta con las letras del alfabeto
+    diccionario="abcdefghijklmnopqrstuvwxtz"
+    #La variable auxiliar nos permite guardar la iteraccion para crear el mensaje encriptado
+    auxiliar=""
+    for i in mensaje:
+        #se lee cada elemento  de la variable  para buscarlo en la variable diccionario se restan 13 ya que esa es la clave del cifrado y usa el modulo para estar iterando en el diccionario
+        auxiliar = auxiliar +  diccionario[(diccionario.find(i) - 13) % len(diccionario)]
+    print(auxiliar)
+
+if __name__ == "__main__":
+   cifradoCesar13(input("Escriba su mensaje sin espacios ni la letra ñ: "))
+ #descifradoCesar13("pbzbrfgnf")
